5c0f9f0cfd9b266fdecf5ed6b8a8a940526776b1
“Jackie <56136954+jackiespear@users.noreply.github.com>
2024-02-12 14:05:38 -0600

    Update to final version
    updating to final version

--- stimuli/study_words_inconsist_practice.js
+++
@@ -1,4 +0,0 @@
-studywords_inconsist_practice = [
-{"stimulus":"<p style='font-size: 48px;'>FREEDOM</p>","data":{"type":"practice","category":"inconsistent","word":"FREEDOM"}},
-{"stimulus":"<p style='font-size: 48px;'>ETHICS</p>","data":{"type":"practice","category":"inconsistent","word":"ETHICS"}},
-]
